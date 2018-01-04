5d30be41d33106a275e746b063d27b6383647bfd
Vicuko <victorc.fernandez@gmail.com>
2018-01-04 13:09:15 -0700

    Modify view controller name and class name

--- PitchPerfect/PitchPerfect/RecordSoundViewController.swift
+++ PitchPerfect/PitchPerfect/RecordSoundViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordSoundViewController.swift
 //  PitchPerfect
 //
 //  Created by Vicuko on 1/1/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecordSoundViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
