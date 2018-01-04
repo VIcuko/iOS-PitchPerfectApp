ac9ae7cf5ac15c5d6c19c3da19e8996bfba37bdd
Vicuko <victorc.fernandez@gmail.com>
2018-01-04 13:21:05 -0700

    Update recordAudio function

--- PitchPerfect/PitchPerfect/RecordSoundViewController.swift
+++ PitchPerfect/PitchPerfect/RecordSoundViewController.swift
@@ -32,6 +32,18 @@ class RecordSoundViewController: UIViewController {
         stopRecordingButton.isEnabled = true
         recordingLabel.text = "Recording in Progress"
         
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()        
     }
     @IBAction func stopRecording(_ sender: Any) {
         stopRecordingButton.isEnabled = false
